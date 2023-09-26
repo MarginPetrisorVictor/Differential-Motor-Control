8efbcc3ff30f0f5d0ce87d85cd2f42aacc1b874e
Petrisor <petrisor.margin@yahoo.com>
2023-09-26 14:00:03 +0300

    Modified PWM to see if it generates a 50Hz signal

--- CommandUnit/CommandUnit.ino
+++ CommandUnit/CommandUnit.ino
@@ -1,39 +1,6 @@
+// AICI O SA FIE PUS CODUL FINAL:
 
-void init_Adc(){
-  ADMUX = 0b01000000;         //Setez tensiunea de referinta (AVcc)
-  ADCSRA |= (1<<7);           //Enable la convertorul analogic digital
-}
-
-int read_Adc(char channel){
-  ADMUX &= 0b01000000;        //Resetez canalul de convertire
-  ADMUX |= channel;           //Setez canalul de pe care citesc
-  ADCSRA |= (1<<6);           //Pornesc conversia
-  while(ADCSRA & (1<<6))      //Astept finalul conversiei
-  return (ADCL | (ADCH << 8));
-}
-
-void init_pwm(){              //Timer 1            
-  DDRB |= (1<<1);             //Setez iesire pwm PB1 (pinul 9)
-  TCCR1A = 0b10000010;        //Counter normal
-  TCCR1B = 0b00011001;        //Prescaler de 1024
-  ICR1 = 39999;               //Top value
-  TCNT1 = 0;                  //Counter curent
-  OCR1A = 0;                  //Valoarea la care se modifica starea (0 -> 1)
-  OCR1B = 0;
-}
-
-void set_pwm(float dutyRatio){
-  OCR1A = ICR1 * dutyRatio;
-}
-
-void setup() {
-  Serial.begin(115200);
-  init_Adc();
-  init_pwm();
-}
-
-void loop() {
-  float input_value = read_Adc(0);
-  float dutyRatio = input_value/1023;
-  set_pwm(dutyRatio);
-}
+// 1.Pozitia si directia data de quadratic encoder (DONE);
+// 2.Generarea de semnal PWM;
+// 3.Masurarea vitezei motorului/motoarelor;
+// 4.Regulatorul pentru comanda motoarelor;
--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -1,27 +1,41 @@
-int s = 0;
-int c = 1;
-void init_timer(){
-  TCCR0A = 0b00000000;  //Counter normal
-  TCCR0B = 0b00000101;  //Prescaler de 1024
-  TCNT0 = 0;
+// GENEREAZA UN SEMNAL PWM BAZAT PE CE CITESTE DE PE UN POTENTIOMETRU;
+// De masurat frecventa cu osciloscop pentru verificare precisa!
+
+void init_Adc(){
+  ADMUX = 0b01000000;         //Setez tensiunea de referinta (AVcc)
+  ADCSRA |= (1<<7);           //Enable la convertorul analogic digital
+}
+
+int read_Adc(char channel){
+  ADMUX &= 0b01000000;        //Resetez canalul de convertire
+  ADMUX |= channel;           //Setez canalul de pe care citesc
+  ADCSRA |= (1<<6);           //Pornesc conversia
+  while(ADCSRA & (1<<6))      //Astept finalul conversiei
+  return (ADCL | (ADCH << 8));
+}
+
+void init_pwm(){              //Timer 1            
+  DDRB |= (1<<1);             //Setez iesire pwm PB1 (pinul 9)
+  TCCR1A = 0b10000010;        //Counter normal
+  TCCR1B = 0b00011100;        //Prescaler de 256;
+  ICR1 = 1250;                //Top value
+  TCNT1 = 0;                  //Counter curent
+  OCR1A = 0;                  //Valoarea la care se modifica starea (0 -> 1)
+  OCR1B = 0;
+}
+
+void set_pwm(float dutyRatio){
+  OCR1A = ICR1 * dutyRatio;
 }
 
 void setup() {
   Serial.begin(115200);
-  init_timer();
+  init_Adc();
+  init_pwm();
 }
 
 void loop() {
-   if(TCNT0 >= 78){
-    TCNT0 = 0;
-    c++;
-  }
-  
-  if(TCNT0 >= 20){
-    s = 1;
-  }
-  else 
-    s = 0;
-  if(c <= 50)
-    Serial.println(s);
+  float input_value = read_Adc(0);
+  float dutyRatio = input_value/1023;
+  set_pwm(dutyRatio);
 }
