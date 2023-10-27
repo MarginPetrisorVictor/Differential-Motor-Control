27f25693b11b619e75e32220312787ec0b4d3d1f
Petrisor <petrisor.margin@yahoo.com>
2023-10-27 17:55:52 +0300

    Modified Ackermann model

--- Ackermann Model/Params.m
+++ Ackermann Model/Params.m
@@ -1,5 +1,6 @@
 % De calibrat dimensiuni echivalente a unui porsche taycan:
 
-L = 4;         % Distanta dintre centrele rotilor din fata si spate
-T = 2;         % Distanta dintre centrele rotilor alaturate
-lamda = 0.15;  % Slip ratio
+L = 4;            % Distanta dintre centrele rotilor din fata si spate
+T = 2;            % Distanta dintre centrele rotilor alaturate
+Lamda_in = 0.15;  % Slip ratio for the inner wheel
+Lamda_out = 0.1;  % Slip ratio for the outside wheel
