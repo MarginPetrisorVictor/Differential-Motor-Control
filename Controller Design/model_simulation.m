1fdf8f27224e5d968e29a4c0e716b99f316ef9bd
Petrisor <petrisor.margin@yahoo.com>
2023-10-28 16:35:00 +0300

    New data for motor identification

--- Controller Design/model_simulation.m
+++ Controller Design/model_simulation.m
@@ -11,7 +11,28 @@
 
 viteza = DateIdentificare(:,1)';
 u = DateIdentificare(:,2)';
-t = 0:1:130;
+t = 0:1:4858;
 
-date_identificare = iddata(u',viteza',0.01);
-model = oe(date_identificare,[1 1 1]);
+viteza_treapta = viteza(1,1:858);
+u_treapta = u(1,1:858);
+t = 0:857;
+
+viteza_treapta = medfilt1(viteza_treapta,85);
+u_treapta = medfilt1(u_treapta,85);
+
+plot(t,viteza_treapta,t,u_treapta*6500);
+
+T = 47.5/1000;
+Tm = 35/1000;
+K = 862.89;
+
+t = 0:4857;
+
+H = tf(K,[T 1],'IODelay', Tm);
+u = u(1,1:4858);
+viteza = viteza(1,1:4858);
+system = lsim(H,viteza,t);
+
+plot(t,system,t,viteza);
+%date_identificare = iddata(u',viteza',0.01);
+%model = oe(date_identificare,[1 1 1]);
